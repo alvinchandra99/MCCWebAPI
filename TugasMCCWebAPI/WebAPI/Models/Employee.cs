a69ea1eef0ac55440a3aef6aa63872b24c241c1f
alvinchandra99 <74279384+alvinchandra99@users.noreply.github.com>
2020-11-30 23:03:58 +0700

    Tugas MCC

---
+++ TugasMCCWebAPI/WebAPI/Models/Employee.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebAPI.Models
+{
+    public class Employee
+    {
+        public Employee() { }
+        public Employee(int id, string name)
+        {
+            Id = id;
+            Name = name;
+           
+        }
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+       
+        
+        [ForeignKey("department")]
+        public int DepartementID { get; set; }
+        public Department department { get; set; }
+        
+        //public virtual Department Departments { get; set; }
+    }
+}
